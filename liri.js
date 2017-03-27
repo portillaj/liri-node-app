3d582ddeafe0da8ec862299038ddd8a5759c721f
Jason Portilla <jasonportilla@Jasons-MacBook-Pro.local>
2017-03-27 16:59:00 -0400

    finished api, need to fix when user enter  and read from txt file

--- liri.js
+++ liri.js
@@ -2,6 +2,8 @@
 var myTwitter = require("./keys.js");
 var Twitter = require('twitter');
 var spotify = require('spotify');
+var request = require('request');
+
 var consumerKey = myTwitter.twitterKeys.consumer_key;
 var consumerSecret = myTwitter.twitterKeys.consumer_secret;
 var accessToken = myTwitter.twitterKeys.access_token_key;
@@ -15,42 +17,86 @@ var client = new Twitter({
 });//end client Twitter constructor
 
 var spotifySongArr = [];
+var movieArr = [];
 
 //when the user types my-tweets
 var user = process.argv[2];
-var searchSpotify = process.argv;
+var search = process.argv;
 
 //the maximum number of tweets to display
 var count = 20;
-
+    //when user enters my-tweets for current tweets
     if (user === "my-tweets") {
         client.get('statuses/user_timeline.json?screen_name=portillaj20&' + count, function (error, tweets, response) {
-
-            for (var i = 0; i < count; i++) {
+            //console.log(JSON.stringify(tweets, null, 2));
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("===================Tweet:" + (i + 1) + "======================");
                 console.log(JSON.stringify(tweets[i].created_at, null, 2));
                 console.log(JSON.stringify(tweets[i].text, null, 2));
-                console.log("");
+                console.log("================================================");
             }//end for loop
 
         });//end function
-    } else if(user === "spotify-this-song" && searchSpotify != ""){
-        for(var i = 3; i < searchSpotify.length; i++){
-            spotifySongArr.push(searchSpotify[i]);
+    } 
+
+    //when user enters a song title
+    if(user === "spotify-this-song" && process.argv[3] !== ""){
+        
+        for(var i = 3; i < search.length; i++){
+            spotifySongArr.push(search[i]);
         }//end for loop
         var getSong = spotifySongArr.join(" ");
-
+        //console.log(getSong);
+        //spotify api call
         spotify.search({ type: 'track', query: getSong }, function(err, data) {
             if ( err ) {
                 console.log('Error occurred: ' + err);
                 return;
             }
+                console.log("=========================================");
+                //artist name
+                console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+                //song name
+                console.log("Song Title: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+                console.log("Album Name: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+                //preview link of the song
+                console.log("Preview Song: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+                console.log("=========================================");
+            });//end function
+    }
 
-            console.log(JSON.stringify(data, null, 2));
-            // items[].artists;
-            // tracks.name;
+    //when user enters movie title
+    if(user === "movie-this" && process.argv[3] !== ""){
+        for(var i = 3; i < search.length; i++){
+            movieArr.push(search[i]);
+        }//end for loop
+        var getMovie = movieArr.join(" ");
+    
+      request('http://www.omdbapi.com/?t=' + getMovie, function (error, response, body) {
+      // console.log('error:', error); // Print the error if one occurred 
+      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+      
+      console.log("=============================================================");
+      //Movie title 
+      console.log("Movie Title: " + JSON.parse(body).Title);
+      //Year the moive came out
+      console.log("Year Released: " + JSON.parse(body).Year);
+      //IMDB Rating of the movie
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      //Country where the movie was produced
+      console.log("Country Produced: " + JSON.parse(body).Country);
+      //Language of the Movie
+      console.log("Language of Movie: " + JSON.parse(body).Language);
+      //Plot of Movie
+      console.log("Movie Plot: " + JSON.parse(body).Plot);
+      //Actors in the movie
+      console.log("Actors: " + JSON.parse(body).Actors);
+      //rotten tomatoes rating
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      //rotten tomatoes website
+      console.log("Rotten Tomatoes Website: https://www.rottentomatoes.com/");
+      console.log("=============================================================");
+    });//end movie function
+}//end if
 
-        });
-}
-
-console.log(getSong);
 
